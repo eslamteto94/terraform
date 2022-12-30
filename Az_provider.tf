e9c9c6e8d87be015d4e263a9d34b8b56c96ab3a1
eslamteto94 <121578020+eslamteto94@users.noreply.github.com>
2022-12-30 09:52:22 +0200

    Add files via upload

---
+++ Az_provider.tf
@@ -0,0 +1,65 @@
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "RG" {
+  name     = "Eslam-Terraform"
+  location = "West Europe"
+
+  tags = {
+
+    Owner = "Eslam"
+
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Eslam-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "VMssubnet"
+  resource_group_name  = azurerm_resource_group.RG.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "TF-VM"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
